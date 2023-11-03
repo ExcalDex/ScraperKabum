58e3e4cbb6d09c15086bfdd7bf261bfff44ac4f9
André Santos <122818731+ExcalDex@users.noreply.github.com>
2023-11-03 00:33:38 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,16 +3,14 @@ import json
 
 if __name__ == '__main__':
     s: ScraperKabum = ScraperKabum("placa-de-video", multiprocessing=False)
-    # links: list[str] = s.get_all_pages() #Comment this if you have a links file already
-    links: list[str] = list() 
-    with open('links_GPU_Kabum.txt', 'r') as f:
-        links = (f.read()).split('\n')
+    links: list[str] = s.get_all_pages() #Comment this if you have a links file already
+    # links: list[str] = list() 
+    # with open('links_GPU_Kabum.txt', 'r') as f:
+        # links = (f.read()).split('\n')
 # Uncomment to run a file that already has all the links you want ^
-    # with open('links_GPU_Kabum.txt', 'w') as f:#Comment this if you have a links file already
-    #    for link in links: #Comment this if you have a links file already
-    #         f.write(f'{link}\n') #Comment this if you have a links file already
-
-
+    with open('links_GPU_Kabum.txt', 'w') as f:#Comment this if you have a links file already
+        for link in links: #Comment this if you have a links file already
+            f.write(f'{link}\n') #Comment this if you have a links file already
             
     data: dict[str, list[str | float]] =  s.get_page_contents(links)
     with open('gpu_kabum.json', 'w') as f:
