a82626cf0179997b9b7365b81f5f724b10601c00
André Santos <122818731+ExcalDex@users.noreply.github.com>
2023-11-14 19:09:09 -0300

    Update ScrapeKabum.py

--- ScrapeKabum.py
+++ ScrapeKabum.py
@@ -1,79 +1,61 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+import requests
+from lxml import html
 
 class ScraperKabum():
-
     def __init__(self, produto:str) -> None:
         self.__produto: str = produto
-        self.__data: dict[str, str | float] = {}
-        self.chrome_service = Service(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=self.chrome_service)
-        self.driver.get(f'https://www.kabum.com.br/busca/{self.__produto.replace(" ", "-")}')
-
+        self.__data: dict[str, list[str]] = {'Nome' : [], 'Valor' : [], 'Link': []}
+    
     def get_all_pages(self) -> list[str]:
-        """
-        Returns:
-            list[str]: list with all of the links which contain the desired product.
-        """
         links: list[str] = list()
         j: int = 2
         times_ten_correction = 0
+        page = requests.get(f'https://www.kabum.com.br/busca/{self.__produto.replace(" ", "-")}')
         while True:
-            try:
-                element = self.driver.find_element('xpath', '//*[@id="listingEmpty"]/b')
-                if element.text == "Lamentamos, nenhum produto encontrado com esse critério de pesquisa.":
-                    break
-            except:
-                ...
+            titulo_acabou = html.fromstring(page.content).xpath('//*[@id="listingEmpty"]/b')
+            if len(titulo_acabou) > 0:
+                break
             for i in range(1, 21):
-                try:
-                    links.append(self.driver.find_element('xpath', f'//*[@id="listing"]/div[3]/div/div/div[2]/div[1]/main/div[{i}]/a').get_attribute('href'))
-                except:
-                    ...
-            try:
-                if(j != 2):
-                    vaux = f"?&page_number={j}"
-                    self.driver.get(f"{self.driver.current_url[:-len(vaux)+times_ten_correction]}?&page_number={j}")
-                    if j == 9 or j == 99 or j == 999:
-                        times_ten_correction = 1
-                    elif times_ten_correction != 0:
-                        times_ten_correction = 0
+                link = html.fromstring(page.content).xpath(f'//*[@id="listing"]/div[3]/div/div/div[2]/div[1]/main/div[{i}]/a/@href')
+                if len(link) != 0:
+                    print(link)
+                    links.append(f'https://www.kabum.com.br{link}'.replace('[', "").replace("]", "").replace(r"'", ""))
                 else:
-                    self.driver.get(f"{self.driver.current_url}?&page_number=2")
-            except:
-                ...
+                    break
+            if(j != 2):
+                vaux = f"?&page_number={j}"
+                page = requests.get(f"{page.url[:-len(vaux)+times_ten_correction]}?&page_number={j}")
+                if j == 9 or j == 99 or j == 999:
+                    times_ten_correction = 1
+                elif times_ten_correction != 0:
+                    times_ten_correction = 0
+            else:
+                page = requests.get(f"{page.url}?&page_number=2")
             j += 1
-        
+
         return links
 
-    def get_page_contents(self, links: list[str]) -> dict[str, list[str | float]]:
-        data: dict[str, list[str | float]] = {'Nome' : [], 'Valor' : [], 'Link': []}
+    def get_pages_contents(self, links: list[str]) -> dict:
         contador: int = 0
         for link in links:
-            try:
-                self.driver.get(link)
-                data['Link'].append(link)
-                try:
-                    element = self.driver.find_element('xpath', '//*[@id="__next"]/main/article/section/div[3]/div[1]/div/h1')
-                    print(link)
-                    data['Nome'].append(element.text)
-                    try:
-                        element = self.driver.find_element('xpath', '//*[@id="blocoValores"]/div[2]/div[1]/div/h4')
-                        print(data['Nome'][-1])
-                        data['Valor'].append(element.text)
-                        print(element.text)
-                    except:
-                        del data['Link'][-1]
-                        del data['Nome'][-1]
+            if link == '':
+                break
+            page = requests.get(link)
+            tree = html.fromstring(page.content)
+            nome: str = tree.xpath('//*[@id="__next"]/main/article/section/div[3]/div[1]/div/h1/text()')
+            if len(nome) == 0:
+                nome = tree.xpath('//*[@id="__next"]/main/article/section/div[2]/div[1]/div/h1/text()')
+                if len(nome) != 0:
+                    valor: str = tree.xpath('//*[@id="blocoValores"]/div[2]/div[1]/div/h4/text()')
+                    if len(valor) != 0:
+                        self.__data['Nome'].append(nome)
+                        self.__data['Valor'].append(valor)
+                        self.__data['Link'].append(link)
+                        print(nome)
+                    else:
                         contador += 1
-                        if contador == 30:
+                        if contador >= 30:
                             break
-                        print("100 preco")
-                except:
-                    del data['Link'][-1]
-                    print("100 nome")
-            except:
-                print("100 link")
-        return data
+
+        return self.__data
     
